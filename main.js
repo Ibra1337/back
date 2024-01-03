a21e96deb321ac9cad95d9d23ebb8616ddd546c0
ibra1337 <ibrahim.jadou@gmail.com>
2024-01-03 03:43:33 +0100

    final

--- main.js
+++ main.js
@@ -1,32 +1,42 @@
 const express = require('express');
-const router = require('./router');
 const http = require('http');
 const socketIO = require('socket.io');
-const dbHandler = require('./dbHandler')
-
-
+const cors = require('cors');
+const dbHandler = require('./dbHandler');
+const router = require('./router');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-app.listen(8080);
-
 app.set('view engine', 'ejs');
+
+app.use(cors());
+
+
 const db = dbHandler.initialize();
 app.use(async (req, res, next) => {
-    // Attach the db variable to the request object
     req.db = db;
     next();
 });
 
-const timestampToUpdate = new Date('2023-01-01T12:00:00Z').getTime(); // Replace with your desired timestamp
-setInterval(() => {
-    const currentTime = Date.now();
-    if (currentTime >= timestampToUpdate) {
-        io.emit('updateEvent', { message: 'Update has occurred!' });
-    }
-}, 1000); // Check every second for the update event
+
+app.use(express.static(__dirname + '/public'));
+
+// Parse incoming requests with URL-encoded payloads
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use((req, res, next) => {
+    res.locals.path = req.path;
+    next();
+});
+
+
+app.get("/", (req, res) => {
+    res.redirect("/register");
+});
+
 
 io.on('connection', (socket) => {
     console.log('A user connected');
@@ -36,19 +46,24 @@ io.on('connection', (socket) => {
     });
 });
 
-console.log( __dirname + '/public');
 
-app.use(express.static( __dirname + '/public'));
-app.use(express.urlencoded({ extended: true }));
+app.use(router.router);
 
-app.use((req, res, next) => {
-    res.locals.path = req.path;
-next();
+// Start the server
+const PORT = 8080;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
-app.get("/" , (req, res) =>
-{
-    res.redirect("/register");
-}) 
 
-app.use(router.router)
+let timestampToUpdate = new Date('2023-01-01T12:00:00Z').getTime(); 
 
+setInterval(async () => {
+    const currentTime = Date.now();
+    if (currentTime >= timestampToUpdate) {
+        // Asynchronously fetch the top 5 data from the database
+        const top5 = await dbHandler.getTop5(db);
+
+
+        io.emit('updateEvent', top5);
+    }
+}, 1000);
