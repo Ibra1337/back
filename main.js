c4cb0b65193deb0e15c5bbd3ba943439d8ffe12c
ibra1337 <ibrahim.jadou@gmail.com>
2024-01-07 15:14:54 +0100

    merge solved

--- main.js
+++ main.js
@@ -1,42 +1,39 @@
 const express = require('express');
+const router = require('./router');
 const http = require('http');
 const socketIO = require('socket.io');
-const cors = require('cors');
-const dbHandler = require('./dbHandler');
-const router = require('./router');
+const dbHandler = require('./dbHandler')
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
+let secretKey = fs.readFileSync('./theBestSecuredPrivateKeyEver', 'utf8');
+
+app.use(cookieParser());
+
+app.use(express.json());
+
 app.set('view engine', 'ejs');
 
-app.use(cors());
-
-
+server.listen(8080)
 const db = dbHandler.initialize();
+
 app.use(async (req, res, next) => {
     req.db = db;
     next();
 });
 
-
-app.use(express.static(__dirname + '/public'));
-
-// Parse incoming requests with URL-encoded payloads
-app.use(express.urlencoded({ extended: true }));
-
-
-app.use((req, res, next) => {
-    res.locals.path = req.path;
-    next();
-});
-
-
-app.get("/", (req, res) => {
-    res.redirect("/register");
-});
-
+const timestampToUpdate = new Date('2023-01-01T12:00:00Z').getTime(); 
+setInterval(() => {
+    const currentTime = Date.now();
+    if (currentTime >= timestampToUpdate) {
+        io.emit('updateEvent', { message: 'Update has occurred!' });
+    }
+}, 3000); 
 
 io.on('connection', (socket) => {
     console.log('A user connected');
@@ -46,24 +43,40 @@ io.on('connection', (socket) => {
     });
 });
 
+console.log( __dirname + '/public');
 
-app.use(router.router);
+app.use(express.static( __dirname + '/public'));
+app.use(express.urlencoded({ extended: true }));
 
-// Start the server
-const PORT = 8080;
-server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.use((req, res, next) => {
+    res.locals.path = req.path;
+next();
 });
+app.get("/" , async(req, res) =>
+{
+    if (req.cookies) {
+        const verified = jwt.verify(req.cookies.TC , secretKey);
+        const userdto = verified.user;
+        const foundUser = await dbHandler.contains(db,
+            'SELECT * FROM users where name = ? and password = ?',
+            [userdto.name, userdto.password])
 
-let timestampToUpdate = new Date('2023-01-01T12:00:00Z').getTime(); 
-
-setInterval(async () => {
-    const currentTime = Date.now();
-    if (currentTime >= timestampToUpdate) {
-        // Asynchronously fetch the top 5 data from the database
-        const top5 = await dbHandler.getTop5(db);
-
-
-        io.emit('updateEvent', top5);
+            if (foundUser)
+            {
+                req.user = userdto;
+                dbHandler.updateEntranceCounter(db, userdto.name);
+    
+                const token = jwt.sign({ user: userdto }, secretKey, { expiresIn: '7d' });
+                res.cookie('TC', token, { httpOnly: true });
+                res.redirect('/ranking');
+            }
+        console.log('session existas')
+    } else {
+        console.log('session does not exists')
+        res.redirect("/register");
     }
-}, 1000);
+    
+}) 
+
+app.use(router.router)
+
